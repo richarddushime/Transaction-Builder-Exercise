b9c84c89891ce0bb8d691fd32c36a3c824ac20e5
richarddushime <mudaherarich@gmail.com>
2024-02-13 18:55:34 +0300

    Txid update

--- tx_tests.py
+++ tx_tests.py
@@ -3,8 +3,8 @@ from main import *
 
 class TestTransactionBuilder(unittest.TestCase):
     def test_generate_redeem_script(self):
-        lock_hex = "74657374"  # "test" in hexadecimal
-        expected_redeem_script = "a87465737487"  # Corresponding redeem script for OP_SHA256 test OP_EQUAL
+        lock_hex = "427472757374204275696c64657273"  # "test" in hexadecimal
+        expected_redeem_script = "a8427472757374204275696c6465727387"  # Corresponding redeem script for OP_SHA256 test OP_EQUAL
         self.assertEqual(generate_redeem_script(lock_hex), expected_redeem_script)
 
     def test_derive_address(self):
@@ -15,16 +15,16 @@ class TestTransactionBuilder(unittest.TestCase):
     def test_construct_transaction(self):
         output_address = "38FwbzpG5tSqvvDuicyXj1GZ4NBfSjdpDP"  # Based on provided output
         value = 100000
-        expected_transaction_data = "1dummy_txid0110000038FwbzpG5tSqvvDuicyXj1GZ4NBfSjdpDP"
+        expected_transaction_data = "1535dfa3634abac4ed004803331f23144dddf5e237539534e524c9f2d9afa44840110000038FwbzpG5tSqvvDuicyXj1GZ4NBfSjdpDP"
         self.assertEqual(construct_transaction(output_address, value), expected_transaction_data)
 
     def test_construct_spending_transaction(self):
-        previous_txid = "previous_txid"
+        previous_txid = "535dfa3634abac4ed004803331f23144dddf5e237539534e524c9f2d9afa4484"
         previous_vout = 0
-        unlocking_script_hex = "4752210222b23b"  # Based on provided output
+        unlocking_script_hex = "00140293308b68f473a15885ab1776674a3a70c77110"  # Based on provided output
         output_address = "38FwbzpG5tSqvvDuicyXj1GZ4NBfSjdpDP"  # Based on provided output
         value = 50000
-        expected_spending_transaction_data = "1previous_txid04752210222b23b15000038FwbzpG5tSqvvDuicyXj1GZ4NBfSjdpDP"  # Based on provided output
+        expected_spending_transaction_data = "1535dfa3634abac4ed004803331f23144dddf5e237539534e524c9f2d9afa4484011000140293308b68f473a15885ab1776674a3a70c7711015000038FwbzpG5tSqvvDuicyXj1GZ4NBfSjdpDP"  # Based on provided output
         self.assertEqual(construct_spending_transaction(previous_txid, previous_vout, unlocking_script_hex, output_address, value), expected_spending_transaction_data)
 
 if __name__ == '__main__':
